60f681d182ed335b631f0215464465a3b00f19bf
George <gbanis@gmail.com>
2014-10-20 16:05:32 -0400

    Migrated all commands from aws-test.rb file

--- lib/amazon.rb
+++ lib/amazon.rb
@@ -15,16 +15,36 @@ module Amazon
     # Calls the Amazon Product Advertising API
     # Input: string
     # Output: XML (?)
-    def get_request(request)
-      HTTParty.get(request)
+    def get_request(query_params)
+      HTTParty.get(signed_request)
     end
 
     private
+
+    # Takes query parameters hash and returns query with ordered params.
+    # Input: hash of strings
+    # Output: string
+    def unsigned_query(query_params)
+      uri = Addressable::URI.new
+      uri.query_values = {
+        AWSAccessKeyId: ACCESS_KEY.to_s,
+        AssociateTag: query_params[:associate_tag],
+        ItemId: query_params[:item_id],
+        Version: Time.now.getutc.strftime("%F"),
+        Timestamp: Time.now.getutc.strftime("%FT%TZ"),
+        Operation: query_params[:operation],
+        ResponseGroup: query_params[:response_group],
+        Service: "AWSECommerceService"
+      }
+
+      uri.query.gsub(':', '%3A').gsub(',', '%2C') # escape special chars
+    end
+
     # Calculates an RFC 2104-compliant HMAC with the SHA256 hash algorithm using the SECRET_KEY
     # Input: string
     # Output: string
-    def generate_signature(unsigned_string)
-      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, SECRET_KEY, unsigned_string)).gsub('+', '%2B').gsub('+', '%3d')
+    def signature(unsigned_request)
+      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, SECRET_KEY, unsigned_request)).gsub('+', '%2B').gsub('+', '%3d')
     end
 
     # Creates the final, signed HTTP request
