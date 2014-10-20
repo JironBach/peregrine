e843951e177e3d62bbedb7fcf30ce6690693cc46
George <gbanis@gmail.com>
2014-10-20 17:01:08 -0400

    Amazon module works. Must refactor and make dynamic

--- lib/amazon.rb
+++ lib/amazon.rb
@@ -13,59 +13,44 @@ module Amazon
 
 
   class Amazon
+
     def initialize
     end
-    # Calls the Amazon Product Advertising API
-    # Input: string
-    # Output: XML (?)
-    def get_request(query_params)
-      HTTParty.get(signed_request(unsigned_query(query_params)))
-    end
 
-    # Takes query parameters hash and returns query with ordered params.
-    # Input: hash of strings
-    # Output: string
-    def unsigned_query(query_params)
+    def query
       uri = Addressable::URI.new
+
       uri.query_values = {
         AWSAccessKeyId: ACCESS_KEY.to_s,
-        AssociateTag: query_params[:associate_tag],
-        ItemId: query_params[:item_id],
+        AssociateTag: "georbani-20",
+        ItemId: "B00I9GYG8O",
         Version: Time.now.getutc.strftime("%F"),
         Timestamp: Time.now.getutc.strftime("%FT%TZ"),
-        Operation: query_params[:operation],
-        ResponseGroup: query_params[:response_group],
+        Operation: "ItemLookup",
+        ResponseGroup: "ItemAttributes,Images,Reviews,SalesRank",
         Service: "AWSECommerceService"
       }
 
-      uri.query.gsub(':', '%3A').gsub(',', '%2C') # escape special chars
+      uri.query.gsub(':', '%3A').gsub(',', '%2C')
     end
 
-    # Calculates an RFC 2104-compliant HMAC with the SHA256 hash algorithm using the SECRET_KEY
-    # Input: string
-    # Output: string
-    def signature(unsigned_request)
-      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, SECRET_KEY, unsigned_request)).gsub('+', '%2B').gsub('+', '%3d')
+    def unsigned_request(query)
+      ['GET', 'webservices.amazon.com', '/onca/xml', query].join("\n")
     end
 
-    # Creates the final, signed HTTP request
-    # Input: string
-    # Output: string
-    def signed_request(unsigned_query)
-      unsigned_request = ['GET', 'webservices.amazon.com', '/onca/xml', unsigned_query].join("\n")
-      signed_request = "http://webservices.amazon.com/onca/xml?" + unsigned_query + "&Signature=#{signature(unsigned_request)}"
+    def generate_signature(unsigned_string)
+      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, SECRET_KEY, unsigned_string)).gsub('+', '%2B').gsub('+', '%3d')
+    end
+
+    def request
+      "http://webservices.amazon.com/onca/xml?" + query + "&Signature=#{generate_signature(unsigned_request(query))}"
+    end
+
+    def response
+      HTTParty.get(request)
     end
   end
 
   a = Amazon.new
-
-  query_params = {
-    associate_tag: "georbani-20",
-    item_id: "B00I9GYG8O",
-    operation: "ItemLookup",
-    responsne_group: "ItemAttributes,Images,Reviews,SalesRank"
-  }
-  binding.pry
-  a.get_request(query_params)
+  puts a.response
 end
-
