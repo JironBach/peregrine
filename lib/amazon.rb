db0ce2cd9d724a917e3260ed02fd0b88e7ae54da
George <gbanis@gmail.com>
2014-10-20 16:26:57 -0400

    Cleaned it up. Still request doesn't go through

--- lib/amazon.rb
+++ lib/amazon.rb
@@ -5,21 +5,22 @@
 module Amazon
   require 'HTTParty'
   require 'cgi'
+  require 'pry'
   require "addressable/uri"
 
   ACCESS_KEY = ENV["AMAZON_ACCESS_KEY"]
   SECRET_KEY = ENV["AMAZON_SECRET_KEY"]
 
-  Class Amazon
 
+  class Amazon
+    def initialize
+    end
     # Calls the Amazon Product Advertising API
     # Input: string
     # Output: XML (?)
     def get_request(query_params)
-      HTTParty.get(signed_request)
+      HTTParty.get(signed_request(unsigned_query(query_params)))
     end
-
-    private
 
     # Takes query parameters hash and returns query with ordered params.
     # Input: hash of strings
@@ -50,9 +51,21 @@ module Amazon
     # Creates the final, signed HTTP request
     # Input: string
     # Output: string
-    def signed_request(query)
-      unsigned_request = ['GET', 'webservices.amazon.com', '/onca/xml', query].join("\n")
-      "http://webservices.amazon.com/onca/xml?" + query + "&Signature=#{generate_signature(unsigned_request)}"
+    def signed_request(unsigned_query)
+      unsigned_request = ['GET', 'webservices.amazon.com', '/onca/xml', unsigned_query].join("\n")
+      signed_request = "http://webservices.amazon.com/onca/xml?" + unsigned_query + "&Signature=#{signature(unsigned_request)}"
     end
   end
+
+  a = Amazon.new
+
+  query_params = {
+    associate_tag: "georbani-20",
+    item_id: "B00I9GYG8O",
+    operation: "ItemLookup",
+    responsne_group: "ItemAttributes,Images,Reviews,SalesRank"
+  }
+  binding.pry
+  a.get_request(query_params)
 end
+
