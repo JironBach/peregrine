8df71f1384e54c373481aff58b7400d16ac174c4
George <gbanis@gmail.com>
2014-10-21 09:36:28 -0400

    Created basic show for link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -18,6 +18,9 @@ class LinksController < ApplicationController
     redirect_to @link
   end
 
+  def show
+    @link = Link.find(params[:id])
+  end
   private
   def link_params
     params.require(:link).permit(:aff_tag, :amzn_url)
