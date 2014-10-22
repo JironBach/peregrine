8a72f358c5cdbcbbf650cdd7569ec86df618b125
George <gbanis@gmail.com>
2014-10-22 16:45:19 -0400

    Added user model and migrated

--- db/migrate/20141021005722_create_sales_ranks.rb
+++
@@ -1,9 +0,0 @@
-class CreateSalesRanks < ActiveRecord::Migration
-  def change
-    create_table :sales_ranks do |t|
-      t.integer :rank
-      t.belongs_to :link, index: true
-      t.timestamp
-    end
-  end
-end
--- db/migrate/20141021141906_drop_sales_ranks_table.rb
+++
@@ -1,4 +0,0 @@
-class DropSalesRanksTable < ActiveRecord::Migration
-  def change
-  end
-end
