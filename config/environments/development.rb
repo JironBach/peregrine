d494c3ce570a5738e8fc7f72733bb8f40f064a62
George <gbanis@gmail.com>
2014-10-22 22:13:10 -0400

    Setting up devise mailer with zoho

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,6 @@
 Rails.application.configure do
 
-  # Devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
-
+  config.action_mailer.default_url_options = { :host => 'smtp.zoho.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
